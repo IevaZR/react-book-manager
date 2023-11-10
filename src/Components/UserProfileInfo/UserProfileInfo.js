bed2677207d7301c4969f662e8db135c23fa2f27
Zvanitaja Rubine <i.zvanitaja.rubine@accenture.com>
2023-11-10 16:38:52 +0200

    user profile user info section html and css

--- src/Components/UserProfileInfo/UserProfileInfo.js
+++ src/Components/UserProfileInfo/UserProfileInfo.js
@@ -1,10 +1,40 @@
-import React from 'react'
-import "./UserProfileInfo.css"
+import React from "react";
+import "./UserProfileInfo.css";
+import EditIcon from "./../../Assets/edit-icon.png";
 
 const UserProfileInfo = () => {
   return (
-    <div>UserProfileInfo</div>
-  )
-}
+    <div className="UserProfileInfoWrapper">
+      <div>
+        <div className="UserProfileInfoDetailWrapper">
+          <p className="UserProfileInfoHeading">Name:</p>
+          <p className="UserProfileInfoValue">User Name</p>
+          <img src={EditIcon} alt="edit-icon" />
+        </div>
+        <div className="UserProfileInfoDetailWrapper">
+          <p className="UserProfileInfoHeading">Surname:</p>
+          <p className="UserProfileInfoValue">User Surname</p>
+          <img src={EditIcon} alt="edit-icon" />
+        </div>
+        <div className="UserProfileInfoDetailWrapper">
+          <p className="UserProfileInfoHeading">Email:</p>
+          <p className="UserProfileInfoValue">user@gmail.com</p>
+          <img src={EditIcon} alt="edit-icon" />
+        </div>
+        <div className="UserProfileInfoDetailWrapper">
+          <p className="UserProfileInfoHeading">Password:</p>
+          <p className="UserProfileInfoValue">***</p>
+          <img src={EditIcon} alt="edit-icon" />
+        </div>
+      </div>
+      <button type="button" className="UserProfileInfoFormBtn">
+        Save and Update
+      </button>
+      <button type="button" className="UserProfileInfoFormDeleteBtn">
+        Delete Profile
+      </button>
+    </div>
+  );
+};
 
-export default UserProfileInfo
+export default UserProfileInfo;
