3dc415fef6fece6f27ed7a1aadcda0bf808a3045
Zvanitaja Rubine <i.zvanitaja.rubine@accenture.com>
2023-11-10 13:46:38 +0200

    user profile button functionality added

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,25 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserProfile.css";
 import UserReadingList from "../UserReadingList/UserReadingList";
 import UserFinishedBooks from "../UserFinishedBooks/UserFinishedBooks";
 import UserProfileInfo from "../UserProfileInfo/UserProfileInfo";
 
 const UserProfile = () => {
+  const [userReadingListVisible, setUserReadingListVisible] = useState(true);
+  const [userFinishedReadingVisible, setUserFinishedReadingVisible] =
+    useState(false);
+  const [userProfileInfoVisible, setProfileInfoVisible] = useState(false);
+
+  const openUserReadingList = () => {
+    setUserReadingListVisible(true);
+    setUserFinishedReadingVisible(false);
+    setProfileInfoVisible(false);
+  };
+  const openUserFinishedList = () => {
+    setUserReadingListVisible(false);
+    setUserFinishedReadingVisible(true);
+    setProfileInfoVisible(false);
+  };
+  const openUserProfileInfo = () => {
+    setUserReadingListVisible(false);
+    setUserFinishedReadingVisible(false);
+    setProfileInfoVisible(true);
+  };
+
   return (
     <div className="UserProfileWrapper">
       <div className="UserProfileMenu">
-        <button className="UserProfileMenuButton List ">My Reading List</button>
-        <button className="UserProfileMenuButton Finished BtnActive">
+        <button
+          className={
+            userReadingListVisible
+              ? "UserProfileMenuButton List BtnActive"
+              : "UserProfileMenuButton List"
+          }
+          onClick={openUserReadingList}
+        >
+          My Reading List
+        </button>
+        <button
+          className={
+            userFinishedReadingVisible
+              ? "UserProfileMenuButton Finished BtnActive"
+              : "UserProfileMenuButton Finished"
+          }
+          onClick={openUserFinishedList}
+        >
           Finsihed reading
         </button>
-        <button className="UserProfileMenuButton Profile">
+        <button
+          className={
+            userProfileInfoVisible
+              ? "UserProfileMenuButton Profile BtnActive"
+              : "UserProfileMenuButton Profile"
+          }
+          onClick={openUserProfileInfo}
+        >
           Update Prodile Info
         </button>
       </div>
       <div className="UserProfileMainSection">
-        <UserReadingList/>
-        <UserFinishedBooks/>
-        <UserProfileInfo/>
+        {userReadingListVisible && <UserReadingList />}
+        {userFinishedReadingVisible && <UserFinishedBooks />}
+        {userProfileInfoVisible && <UserProfileInfo />}
       </div>
     </div>
   );
